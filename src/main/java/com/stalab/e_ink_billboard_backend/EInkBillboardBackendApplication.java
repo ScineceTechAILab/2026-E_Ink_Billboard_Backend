26e3e0731ddc65359de1b7ef9dbd0d4c4542d782
ywz626 <1461507285@qq.com>
2026-01-06 20:49:24 +0800

    init project

---
+++ src/main/java/com/stalab/e_ink_billboard_backend/EInkBillboardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.stalab.e_ink_billboard_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EInkBillboardBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EInkBillboardBackendApplication.class, args);
+    }
+
+}
