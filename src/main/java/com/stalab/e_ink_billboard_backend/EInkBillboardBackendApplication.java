026d6dc70169e142dfef26be00af9d3f24fe138d
ywz626 <1461507285@qq.com>
2026-01-07 16:37:37 +0800

    build: PostgreSQL配置 weixin配置 依赖更新

--- src/main/java/com/stalab/e_ink_billboard_backend/EInkBillboardBackendApplication.java
+++ src/main/java/com/stalab/e_ink_billboard_backend/EInkBillboardBackendApplication.java
@@ -1,9 +1,11 @@
 package com.stalab.e_ink_billboard_backend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.stalab.e_ink_billboard_backend.mapper")
 public class EInkBillboardBackendApplication {
 
     public static void main(String[] args) {
