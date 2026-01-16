3e4c16f0e8722bd8b458092646fdd330d76a5b3c
ywz626 <1461507285@qq.com>
2026-01-16 16:37:06 +0800

    feat(video): 添加视频处理依赖，更新配置文件以支持文件上传

--- src/main/java/com/stalab/e_ink_billboard_backend/EInkBillboardBackendApplication.java
+++ src/main/java/com/stalab/e_ink_billboard_backend/EInkBillboardBackendApplication.java
@@ -3,8 +3,10 @@ package com.stalab.e_ink_billboard_backend;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 @MapperScan("com.stalab.e_ink_billboard_backend.mapper")
 public class EInkBillboardBackendApplication {
 
